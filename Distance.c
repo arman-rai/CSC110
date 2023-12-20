737b8f58fccaa4ef0ad763faaaf864a1f953c50f
arman-rai <118722334+arman-rai@users.noreply.github.com>
2023-12-20 08:14:09 -0500

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -2,13 +2,13 @@
 
 int main() {
 
-	int v, t, a, s;
+	float v, t, a, s;
 
 	printf("Enter the velocity, time and acceleration in SI units:\n");
-	scanf("%d %d %d", &v, &t, &a);
+	scanf("%f %f %f", &v, &t, &a);
 	
 	s = v*t + .5 *a *t*t;
-	printf("The distance covered: %d", s); 
+	printf("The distance covered: %f", s); 
 	
 	return 0;
 
