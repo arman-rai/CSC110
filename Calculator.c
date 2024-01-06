7aca259f90baa67f4aaf28be563b90d83e9444c4
arman-rai <118722334+arman-rai@users.noreply.github.com>
2024-01-06 15:11:24 +0545

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+	float n1, n2;
+	printf("enter the numbers:");
+	scanf("%f%f",&n1,&n2);
+	printf("sum: \t%f", n1+n2);
+	printf("product: \t%f", n1*n2);
+	printf("division: \t%f", n1/n2);
+}
